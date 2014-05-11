fe67085ff4679e689da67097769600f9295db4fc
AJ Norton <ajn123@vt.edu>
2014-05-11 10:28:31 -0400

    added favorite tweeters

---
+++ Tweet.rb
@@ -0,0 +1,21 @@
+=begin
+
+=end
+
+class Tweet
+  attr_reader :text,:favorites,:retweet
+
+
+  def initialize(args)
+    @text = args.fetch(:text,"no text")
+    @favorites = args.fetch(:favorites,"no favorites")
+    @rewteet = args.fetch(:retweet,"no retweets")
+  end
+
+  def to_s
+    "#{@text} favorited: #{@favorites}  retweeted: #{@rewteet}"
+  end
+
+
+end
+
--- TwitterBot.rb
+++ TwitterBot.rb
@@ -1,14 +1,20 @@
 require 'twitter'
+require_relative 'Tweet'
 
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "l3sWtUuWEBVGkC1OrfvnzZCKD"
-  config.consumer_secret     = "Xc3FN1O4xmKjNaUauXzhET2uBUbSu1UgjX6wGLhHaQlwEi9gF3"
-  config.access_token        = "871206637-ftaH5FEBa2NQlr1rcOxvkDLDq35VdkQMKyS3cfcz"
-  config.access_token_secret = "9mJ1T2LVrQ95tdP4HBp3OeWHTnDp01WqbKO5VjAJoqzp0"
+
+favoriteTweeters = ["CNN","kadavy","alexisohanian"]
+
+array = []
+favoriteTweeters.each do |tweeter|
+
+  client.search("from:#{tweeter}",{ :lang =>"en", :result_type => "recent" }).take(10).each do |tweet|
+    array << Tweet.new( {:text => tweet.text, :retweet => tweet.retweeted, :favorites => tweet.favorited})
+
+  end
+
 end
 
-client.search("from:CNN",{ :lang =>"en", :result_type => "recent" }).take(1).each do |tweet|
-  client.update(tweet.text)
-end
 
+
+puts array
