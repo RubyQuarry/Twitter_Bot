d66c8e994046067b48c4a1c64962411b4e14319e
AJ Norton <ajn123@vt.edu>
2014-05-11 11:54:23 -0400

    added trends to twitter bot

--- TwitterBot.rb
+++ TwitterBot.rb
@@ -1,20 +1,38 @@
 require 'twitter'
 require_relative 'Tweet'
+require_relative 'RESTClientInformation' #private twitter information
 
 
 
-favoriteTweeters = ["CNN","kadavy","alexisohanian"]
+class TwitterBot
+  attr_reader :client,:favoriteTweeters, :tweets,:trends
 
-array = []
-favoriteTweeters.each do |tweeter|
+  def initialize
+    @client = Client.new.client
+    @favoriteTweeters = ["CNN","kadavy","alexisohanian"]
+    @tweets = []
+  end
 
-  client.search("from:#{tweeter}",{ :lang =>"en", :result_type => "recent" }).take(10).each do |tweet|
-    array << Tweet.new( {:text => tweet.text, :retweet => tweet.retweeted, :favorites => tweet.favorited})
+  def find_tweets
+    @favoriteTweeters.each do |tweeter|
+      client.search("from:#{tweeter}",{ :lang =>"en", :result_type => "recent" }).take(10).each do |tweet|
+        @tweets << Tweet.new( {:text => tweet.text, :retweet => tweet.retweeted, :favorites => tweet.favorited})
+      end
+    end
+    puts @tweets
+  end
+
+  WOE_ID = 23424977  #USA yahoo area ID
+
+  def find_trends
+    @trends =@client.trends(WOE_ID,{:exclude => "hashtags"})
+    @trends.each do |trend|
+      puts trend.name
+    end
 
   end
 
 end
 
 
-
-puts array
+TwitterBot.new.find_trends
